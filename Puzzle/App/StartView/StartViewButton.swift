6513fd934739781f68e8a4cd7474d267bbd7c52a
Weronika Żywiecka <weronika.zywiecka@nextapps.pl>
2020-07-28 20:59:28 +0200

    start buttons opacity on touch

--- Puzzle/App/StartView/StartViewButton.swift
+++ Puzzle/App/StartView/StartViewButton.swift
@@ -45,13 +45,15 @@ class StartViewButton: UIControl {
             self.imageView.image = image
         }
     }
-//    
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        layer.opacity = 0.7
-//    }
-//
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        layer.opacity = 1
-//    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        layer.opacity = 0.7
+        super.touchesBegan(touches, with: event)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        layer.opacity = 1
+        super.touchesEnded(touches, with: event)
+    }
 
 }
