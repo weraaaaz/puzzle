adc02626d493056d1e501ebef56b1fb3b0d3dbc8
Weronika Żywiecka <weronika.zywiecka@nextapps.pl>
2020-08-18 23:36:54 +0200

    increase/decrese grid count label

--- Puzzle/App/SelectGrid/SelectGridViewController.swift
+++ Puzzle/App/SelectGrid/SelectGridViewController.swift
@@ -12,19 +12,24 @@ class SelectGridViewController: UIViewController, UINavigationControllerDelegate
 
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageBlurView: UIImageView!
     @IBOutlet weak var decreaseCountButton: UIButton!
     @IBOutlet weak var increaseCountButton: UIButton!
-
+    @IBOutlet weak var gridCountLabel: UILabel!
+    
     var image = UIImage()
+
+    var gridCount = 4
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = self.image
+        imageBlurView.image = self.image
         let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        imageView.addSubview(blurEffectView)
+        imageBlurView.addSubview(blurEffectView)
         decreaseCountButton.roundCorners([.topLeft, .bottomLeft], radius: 8)
         increaseCountButton.roundCorners([.topRight, .bottomRight], radius: 8)
     }
@@ -34,9 +39,17 @@ class SelectGridViewController: UIViewController, UINavigationControllerDelegate
     }
 
     @IBAction func onDecreaseButtonTap(_ sender: Any) {
+        if (self.gridCount > 4) {
+            self.gridCount = self.gridCount - 1
+            self.gridCountLabel.text = "Current Grid Size = \(self.gridCount)"
+        }
     }
     
     @IBAction func onIncreaseButtonTap(_ sender: Any) {
+        if (self.gridCount < 12) {
+            self.gridCount = self.gridCount + 1
+            self.gridCountLabel.text = "Current Grid Size = \(self.gridCount)"
+        }
     }
 
 }
